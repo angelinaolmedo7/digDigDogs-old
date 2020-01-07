1a018f6f43b9d6672496e51946294e277d357c37
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-01-07 15:13:18 -0800

    Dog class

---
+++ digDigDogs/DogSprite.swift
@@ -0,0 +1,38 @@
+//
+//  DogSprite.swift
+//  digDigDogs
+//
+//  Created by Angelina Olmedo on 1/7/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class DogSprite: SKSpriteNode {
+    
+    enum DogBreeds:String, CaseIterable {
+        case mutt = "Mutt"
+        case pug = "Pug"
+        case borderCollie = "Border Collie"
+        case australianShepherd = "Australian Shepherd"
+        case lilac = "Dog?"
+    }
+    
+    var dogName: String!
+    var breed: DogBreeds!
+    var dp: Int!
+    
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func generateResource() {
+        print("my dp is \(dp ?? 0)")
+    }
+}
+
